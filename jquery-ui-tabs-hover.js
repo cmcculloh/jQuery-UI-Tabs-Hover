9476a56982b90e21f86f55956091c669efe5979f
Christopher McCulloh <cmcculloh@finishline.com>
2010-08-16 11:40:41 -0400

    Added wrapper for $ that will make plugin safe on pages including libraries other than jQuery

--- jquery-ui-tabs-hover.js
+++ jquery-ui-tabs-hover.js
@@ -1,8 +1,10 @@
-$.extend( $.ui.tabs.prototype, {
-	hover: function(hoverOn, hoverOff) {
-		var self = this,
-			o = this.options;
+;(function($){
+	$.extend( $.ui.tabs.prototype, {
+		hover: function(hoverOn, hoverOff) {
+			var self = this,
+				o = this.options;
 
-		self.element.find("li a").hover(hoverOn,hoverOff);
-	}
-});
+			self.element.find("li a").hover(hoverOn,hoverOff);
+		}
+	});
+})(jQuery);
